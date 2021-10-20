dadceb0464a51b9dfdbed3f55cf08851b66e9910
Jon <jonshaw199@gmail.com>
2021-10-20 11:36:58 -0700

    3+ Users - Modifying models, routes, and DB seeder

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -5,13 +5,13 @@ const Message = require("./message");
 // associations
 
 User.hasMany(Conversation);
-Conversation.belongsTo(User, { as: "user1" });
-Conversation.belongsTo(User, { as: "user2" });
+// Conversation.belongsTo(User, { as: "user1" });
+// Conversation.belongsTo(User, { as: "user2" });
 Message.belongsTo(Conversation);
 Conversation.hasMany(Message);
 
 module.exports = {
   User,
   Conversation,
-  Message
+  Message,
 };
